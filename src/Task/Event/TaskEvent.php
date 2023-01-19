386b665ef3c4a9de30bc1980b8a84ebaec02b740
TheSametCNC <60401648+TheSametCNC@users.noreply.github.com>
2023-01-19 03:17:51 +0300

    Update TaskEvent.php

--- src/Task/Event/TaskEvent.php
+++ src/Task/Event/TaskEvent.php
@@ -3,14 +3,14 @@
 namespace Task\Event;
 
 use pocketmine\Server;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\scheduler\Task;
 use Task\Event\Base;
 use onebone\economyapi\EconomyAPI;
 
 class TaskEvent extends Task{
 	
-	public function onRun(int $currentTick): void{
+	public function onRun(): void{
 		foreach(Server::getInstance()->getOnlinePlayers() as $player){
 			EconomyAPI::getInstance()->addMoney($player->getName(), 200);
 		}
